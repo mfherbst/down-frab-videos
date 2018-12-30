53f11cd395f7066c45a3e3ee2d9da601a4c783e2
Michael F. Herbst <info@michael-herbst.com>
2018-12-30 21:11:47 +0100

    Bump version: 0.5.4 → 0.5.5

--- down_frab_videos/__init__.py
+++ down_frab_videos/__init__.py
@@ -28,7 +28,7 @@ import textwrap
 from .config import config
 
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 __licence__ = "GPL v3"
 __authors__ = "Michael F. Herbst"
 __email__ = "info@michael-herbst.com"
